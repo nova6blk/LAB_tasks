a47b4912429214598950034131daffc6434f1bc6
nova6blk <nova6blk@gmail.com>
2022-10-18 01:13:45 +0300

    Complete task5_2 and 5_2a

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3.10
 # -*- coding: utf-8 -*-
 """
 Задание 5.2
@@ -30,3 +31,58 @@ Out[1]: '11111111111111111111111111110000'
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+
+enter = input('Введите IP адрес и префикс в формате Х.Х.Х.Х/XX: ')
+### IP-address ###
+octet_1 = int(enter.split('/')[0].split('.')[0])
+octet_2 = int(enter.split('/')[0].split('.')[1])
+octet_3 = int(enter.split('/')[0].split('.')[2])
+octet_4 = int(enter.split('/')[0].split('.')[3])
+### Prefix\mask ###
+prefix = int(enter.split('/')[1]) ### /24
+prefixbin = '1' * prefix + '0' * (32 - prefix) ### 11111111111111111111111100000000
+maskbin_1 = prefixbin[0:8]
+maskbin_2 = prefixbin[8:16]
+maskbin_3 = prefixbin[16:24]
+maskbin_4 = prefixbin[24:]
+mask_1 = int(maskbin_1, 2)
+mask_2 = int(maskbin_2, 2)
+mask_3 = int(maskbin_3, 2)
+mask_4 = int(maskbin_4, 2)
+
+template = """
+Network:
+{0:<10} {1:<10} {2:<10} {3:<10}
+{0:>08b}   {1:>08b}   {2:>08b}   {3:>08b}
+
+Mask:
+/{4}
+{5:<10} {6:<10} {7:<10} {8:<10}
+{9:10} {10:10} {11:10} {12:10}
+"""
+print(template.format(octet_1,octet_2,octet_3,octet_4,prefix,mask_1,mask_2,mask_3,mask_4,maskbin_1,maskbin_2,maskbin_3,maskbin_4))
+######################
+### Хуевое решение ###
+######################
+#prefix = int(enter.split('/')[1])
+#prefixbin = '1' * prefix + '0' * (32 - prefix)
+#octet_1 = '{:08b}'.format(int(enter.split('/')[0].split('.')[0])) + ' ' * 2
+#octet_2 = '{:08b}'.format(int(enter.split('/')[0].split('.')[1])) + ' ' * 2
+#octet_3 = '{:08b}'.format(int(enter.split('/')[0].split('.')[2])) + ' ' * 2
+#octet_4 = '{:08b}'.format(int(enter.split('/')[0].split('.')[3]))
+#
+#maskbin_1 = prefixbin[0:8] + ' ' * 2
+#maskbin_2 = prefixbin[8:16] + ' ' * 2
+#maskbin_3 = prefixbin[16:24] + ' ' * 2
+#maskbin_4 = prefixbin[24:] + ' ' * 2
+#mask_1 = int(prefixbin[0:8], 2)
+#mask_2 = int(prefixbin[8:16], 2)
+#mask_3 = int(prefixbin[16:24], 2)
+#mask_4 = int(prefixbin[24:], 2)
+#
+#print('Network:\n' + (' ' * 9).join(enter.split('/')[0].split('.')))
+#print(octet_1, octet_2, octet_3, octet_4)
+#print('\nMask:\n/' + str(prefix))
+#print(mask_1, '      ',mask_2,'      ',mask_3,'      ',mask_4)
+#print(maskbin_1, maskbin_2,maskbin_3,maskbin_4)
--- exercises/05_basic_scripts/task_5_2a.py
+++ exercises/05_basic_scripts/task_5_2a.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3.10
 # -*- coding: utf-8 -*-
 """
 Задание 5.2a
@@ -49,3 +50,37 @@ bin_ip = "00001010000000010000000111000011"
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+enter = input('Введите IP адрес и префикс в формате Х.Х.Х.Х/XX: ')
+prefix = int(enter.split('/')[1]) ### /24
+### IP-address ###
+octet_1bin = '{:08b}'.format(int(enter.split('/')[0].split('.')[0]),'b')
+octet_2bin = '{:08b}'.format(int(enter.split('/')[0].split('.')[1]),'b')
+octet_3bin = '{:08b}'.format(int(enter.split('/')[0].split('.')[2]),'b')
+octet_4bin = '{:08b}'.format(int(enter.split('/')[0].split('.')[3]),'b')
+new_ip_bin = (octet_1bin + octet_2bin + octet_3bin + octet_4bin)[:prefix] + '0' * (32 - prefix)
+octet_1 = int(new_ip_bin[0:8],2)
+octet_2 = int(new_ip_bin[8:16],2)
+octet_3 = int(new_ip_bin[16:24],2)
+octet_4 = int(new_ip_bin[24:],2)
+### Prefix\mask ###
+prefixbin = '1' * prefix + '0' * (32 - prefix) ### 11111111111111111111111100000000
+maskbin_1 = prefixbin[0:8]
+maskbin_2 = prefixbin[8:16]
+maskbin_3 = prefixbin[16:24]
+maskbin_4 = prefixbin[24:]
+mask_1 = int(maskbin_1, 2)
+mask_2 = int(maskbin_2, 2)
+mask_3 = int(maskbin_3, 2)
+mask_4 = int(maskbin_4, 2)
+
+template = """
+Network:
+{0:<10} {1:<10} {2:<10} {3:<10}
+{0:>08b}   {1:>08b}   {2:>08b}   {3:>08b}
+
+Mask:
+/{4}
+{5:<10} {6:<10} {7:<10} {8:<10}
+{9:10} {10:10} {11:10} {12:10}
+"""
+print(template.format(octet_1,octet_2,octet_3,octet_4,prefix,mask_1,mask_2,mask_3,mask_4,maskbin_1,maskbin_2,maskbin_3,maskbin_4))
